d8d6a6ba096b1a888ca38cc635a60cce23d4f6ab
shohjahonsherzodov <uzbuzb314@gmail.com>
2025-08-20 17:01:54 +0500

    upgrade

---
+++ 05-dars/accounts/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+class CustomRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        "placeholder": "Username",
+        "class": "form-input"
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        "placeholder": "Password",
+        "class": "form-input"
+    }))
